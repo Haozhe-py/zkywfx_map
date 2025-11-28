6afaffa495266fb44d84555cc5b41381b9756dbd
Haozhe Xu <xuhaozhepy@outlook.com>
2025-11-28 20:01:33 +0800

    Update

---
+++ js/task.js
@@ -0,0 +1,10 @@
+(function(){
+	width = window.innerWidth;
+    widthPerPicture = width / 4;
+
+    const picture_ids = ['basic', 'poems', 'readings', 'composition']
+    for (const id of picture_ids) {
+        document.getElementById(id).style.width = widthPerPicture + 'px';
+    }
+}
+)();
